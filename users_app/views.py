7347a8f1f3797fb09d84a94a1d392429c22b561a
PythonMade <ronald@pythonmade.com>
2020-07-28 19:39:59 -0400

    create user registration form

--- users_app/views.py
+++ users_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -12,6 +13,6 @@ def register(request):
         return redirect('tweets-app-home')
     else:
         context = {
-            'form': UserCreationForm()
+            'form': UserRegisterForm()
         }
         return render(request, 'users_app/register.html', context)
